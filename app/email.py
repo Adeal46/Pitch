2f34dd0e8c7298773b9f86b9549e933b9c9e435f
Adeal <adililema3@gmail.com>
2018-11-21 22:58:11 +0300

    add the files not to display in the repo

--- app/email.py
+++ app/email.py
@@ -5,6 +5,7 @@ from . import mail
 
 def mail_message(subject,template,to,**kwargs):
     sender_email="adililema3@gmail.com"
+    sender_password="orientally"
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
